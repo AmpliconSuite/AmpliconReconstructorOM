c07b8f065408944821e4377f6fecdca9454d2b99
Jens Luebeck <jluebeck@eng.ucsd.edu>
2018-11-07 11:17:16 -0800

    more bugfixes to setup

--- setup.sh
+++ setup.sh
@@ -2,18 +2,23 @@
 
 #This sets up AmpliconReconstuctor and SegAligner as a local command line utility, callable from any directory
 
-#add AmpliconReconstructor to path
+#add AmpliconReconstructor and SegAligner to path
 echo "export AR_SRC=$PWD" >> ~/.bashrc
-echo "alias AmpliconReconstructor='$AR_SRC/AmpliconReconstructor'" >> ~/.bashrc
-echo "alias AR='$AR_SRC/AmpliconReconstructor'" >> ~/.bashrc
+echo "export SA_SRC=$PWD" >> ~/.bashrc
+source ~/.bashrc
+
+#alias AmpliconReconstructor
+echo "alias AmpliconReconstructor='python2 $AR_SRC/AmpliconReconstructor'" >> ~/.bashrc
+echo "alias AR='python2 $AR_SRC/AmpliconReconstructor'" >> ~/.bashrc
 
 #make SegAligner
 cd SegAligner
 make clean; make
 
-#add SegAligner to path
-echo "export SA_SRC=$PWD" >> ~/.bashrc
+#alias SegAligner
 echo "alias SegAligner='$SA_SRC/SegAligner'" >> ~/.bashrc
 
 #make changes effective for current session
 source ~/.bashrc
+
+echo -e "\nSetup complete\n"
